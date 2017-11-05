7813c69f663554b7ae277586289e51ac5a8d6940
shinjiang <shinjiang1102@gmail.com>
2017-11-05 19:03:38 +0800

    11.5评论

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category
 from django.shortcuts import render, get_object_or_404
+from comments.forms import CommentForm
 import markdown
 # Create your views here.
 
@@ -32,10 +32,26 @@ def detail(request, pk):
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                   ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    # return render(request, 'blog/detail.html', context={'post': post})
+    return render(request, 'blog/detail.html', context=context)
 
+# 归档页面
 def archives(request, year, month):
     post_list = Post.objects.filter(created_time__year = year,
-                                    creat_time__month = month
+                                    created_time__month = month
                                     ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+# 分类页面
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog', # 注册blog 应用
+    'comments', # 注册 comments 应用
 ]
 
 MIDDLEWARE = [
